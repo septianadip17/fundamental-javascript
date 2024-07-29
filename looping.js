9488a7bbf6c8fda65c84079bdf8c1904c79d66fa
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-29 12:45:09 +0700

    belanja

--- looping.js
+++ looping.js
@@ -124,17 +124,97 @@ Outputnya
 //   console.log(star);
 // }
 
+// // /**
+// // 2
+// // 33
+// // 444
+// // 5555
+// // 66666
+// //  */
+// for (let i = 2; i <= 6; i++) {
+//   string = "";
+//   for (let j = 2; j <= i; j++) {
+//     string += i;
+//   }
+//   console.log(string);
+// }
+
 // /**
-// 2
-// 33
-// 444
-// 5555
-// 66666
+// 11111
+// 22222
+// 33333
+// 44444
+// 55555
+
+// Kalau gue masukin 3, berarti 1 nya ada 3 dst
+// Kalau gue masukin 7, berarti 1 nya ada 7 dst
 //  */
-for (let i = 2; i <= 6; i++) {
-  string = "";
-  for (let j = 2; j <= i; j++) {
-    string += i;
-  }
-  console.log(string);
+
+// for (let i = 1; i <= 5; i++) {
+//   string = ""
+//   for (let j = 1; j <= 5; j++) {
+//     string += i
+//   }
+//   console.log(string)
+// }
+
+/**
+
+Aplikasi Kasir:
+
+Barang-barang yang tersedia adalah:
+1. Minyak Goreng 2L Sania (Rp. 35.000)
+2. Tepung 1kg (Rp. 12.000)
+3. Mie Goreng 1pcs (Rp. 3.000)
+4. Saos ABC 1btl (Rp. 15.000)
+5. Voucher Google Play (Rp. 52.000)
+
+Pilih barang yang ingin di beli? (masukkan input)
+
+Masukkan total barang yang ingin di beli? (masukkan input)
+
+Barang yang anda beli adalah Minyak Goreng 2L Sania berjumlah 3,
+total yang harus anda bayarkan adalah Rp. 105.000
+
+ */
+
+console.log("Aplikasi Kasir:");
+console.log("")
+console.log("Barang-barang yang tersedia adalah: ");
+console.log(`1. Minyak Goreng 2L Sania (Rp. 35.000)
+2. Tepung 1kg (Rp. 12.000)
+3. Mie Goreng 1pcs (Rp. 3.000)
+4. Saos ABC 1btl (Rp. 15.000)
+5. Voucher Google Play (Rp. 52.000)`);
+console.log("")
+
+const prompt = require("prompt-sync")();
+barang = Number(prompt("Pilih barang yang ingin dibeli? "));
+console.log("")
+
+var name = "";
+var price = "";
+if (barang == 1) {
+  (name = "Minyak Goreng 2L Sania"), (price = 35000);
+} else if (barang == 2) {
+  (name = "Tepung 1kg"), (price = 12000);
+} else if (barang == 3) {
+  name = "Mie Goreng 1pcs";
+  price = 3000;
+} else if (barang == 4) {
+  name = "Saos ABC 1btl";
+  price = 15000;
+} else if (barang == 5) {
+  name = "Voucher Google Play";
+  price = 52000;
+} else {
+  console.log("Barang yang kamu masukkan tidak ada");
 }
+
+// disini proses mendapatkan total barang yg ingin dibeli
+totalBarang = Number(prompt("Masukkan total barang yang ingin dibeli? "));
+totalBelanja = totalBarang * price
+console.log("")
+
+// disini proses kalkulasi
+console.log(`Barang yang anda beli adalah ${name} berjumlah ${totalBarang}, total yang harus anda bayar adalah Rp. ${totalBelanja}`)
