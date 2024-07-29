149bf6dc9511c69a04143db00aaadb0397441d47
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-29 16:14:11 +0700

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+// // function declaration
+// function jumlahDuaBilangan(a, b){
+//   var total;
+//   total = a + b;
+//   return total
+// }
+// console.log(jumlahDuaBilangan(2,3))
+
+// function expression
+var jumlahDuaBilangan = function (a, b) {
+  var total;
+  total = a + b;
+  return total;
+};
+console.log(jumlahDuaBilangan(3,3))
