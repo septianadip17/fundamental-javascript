434af3e9193ece17cbc4fc3c2530010c9a5c1f73
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-29 12:57:03 +0700

    object clear

--- objects.js
+++ objects.js
@@ -1,7 +1,42 @@
-const example = {
+// const example = {
+//     name: "Rivaldy",
+//     title: "Software Engineer (Backend)",
+//     address: "Jl. Panjang"
+// }
+
+// console.log(example);
+
+/**
+
+var example = {
     name: "Rivaldy",
     title: "Software Engineer (Backend)",
     address: "Jl. Panjang"
 }
 
-console.log(example);
+Masukkan nama:
+Masukkan title:
+Masukkan address:
+
+Gue pengen semua data didalam object tersebut diganti dengan data diri lu.
+Setelah itu di print object nya, dan cek apakah datanya sesuai atau tidak dengan yg lu input.
+
+ */
+
+var example = {
+  name: "Rivaldy",
+  title: "Software Engineer (Backend)",
+  address: "Jl. Panjang",
+};
+const prompt = require("prompt-sync")();
+newName = String(prompt("Masukkan nama: "));
+newTitle = String(prompt("Masukkan title: "));
+newAddress = String(prompt("Masukkan address: "));
+
+example.name = newName;
+example.title = newTitle;
+example.address = newAddress;
+
+console.log(
+  `Halo ${newName}, kamu adalah seorang ${newTitle} dan kamu beralamat di ${newAddress}`
+);
