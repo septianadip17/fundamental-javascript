a6f51e46bdfcdb309d420ab4fd4548700f7ed0b1
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-29 13:06:25 +0700

    array brandCar

---
+++ array.js
@@ -0,0 +1,14 @@
+// ["Saab", "Volvo", "BMW"];
+let brandCar = ["Saab", "Volvo", "BMW"]
+console.log(brandCar)
+
+// gue pengen volvo diganti jadi toyota
+// gue pengen saab diganti jadi honda
+// gue pengen bmw diganti jadi suzuki
+brandCar[0] = "honda"
+brandCar[1] = "toyota"
+brandCar[2] = "suzuki"
+console.log(brandCar)
+
+// print toyota based on brandCar array
+console.log(brandCar[1])
