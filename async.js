a63cf841bb9362199853e605084f16aecb5681de
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-31 15:55:40 +0700

    promise

--- async.js
+++ async.js
@@ -10,12 +10,3 @@
 setTimeout(function () {
   console.log("Ini akan muncul setelah 3 detik");
 }, 3000);
-
-
-
-
-
-// // Menghentikan `setInterval` setelah 5 detik
-// setTimeout(function () {
-//   clearInterval(intervalID);
-// }, 5000);
--- setInterval.js
+++ setInterval.js
@@ -1,4 +1,3 @@
-
 setInterval(() => {
   let now = new Date();
   console.log(now.toLocaleTimeString());
@@ -8,11 +7,34 @@ let count = 0;
 let maxCount = 5;
 
 let intervalID = setInterval(() => {
-    console.log("Ini pesan yang muncul setiap 1 detik");
-    count++;
+  console.log("Ini pesan yang muncul setiap 1 detik");
+  count++;
 
-    if (count >= maxCount) {
-        clearInterval(intervalID);
-        console.log("Interval dihentikan");
-    }
+  if (count >= maxCount) {
+    clearInterval(intervalID);
+    console.log("Interval dihentikan");
+  }
 }, 1000);
+
+function displayMessage(name, message) {
+  console.log(`${name}, ${message}`);
+}
+
+setInterval(
+  displayMessage,
+  3000,
+  "Septian",
+  "ini pesan yang muncul setiap 3 detik!"
+);
+
+setInterval(() => {
+  console.log("Pesan ini muncul setiap 1 detik");
+}, 1000);
+
+let messages = ["Pesan 1", "Pesan 2", "Pesan 3"];
+let index = 0;
+
+setInterval(() => {
+  document.getElementById("message").innerText = messages[index];
+  index = (index + 1) % messages.length;
+}, 2000);
