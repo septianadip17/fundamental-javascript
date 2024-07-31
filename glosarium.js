1c2ab68fb8d50b473fc08f4bd1edafa88720dc9c
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-31 11:59:14 +0700

    continue, break, loop

---
+++ glosarium.js
@@ -0,0 +1,24 @@
+// continue: Melewati iterasi loop saat ini dan melanjutkan dengan iterasi berikutnya.
+for (let i = 0; i < 5; i++) {
+  if (i === 2) {
+    continue; // Melewati iterasi saat i adalah 2
+  }
+  console.log(i);
+}
+
+// // break: Menghentikan loop sepenuhnya dan keluar dari loop.
+// for (let i = 0; i < 5; i++) {
+//   if (i === 2) {
+//     break; // Menghentikan loop saat i adalah 2
+//   }
+//   console.log(i);
+// }
+
+// // return: Menghentikan eksekusi fungsi dan mengembalikan nilai dari fungsi tersebut.
+// function tambah(a, b) {
+//   return a + b; // Mengembalikan hasil penjumlahan
+//   console.log("Ini tidak akan dijalankan");
+// }
+
+// let hasil = tambah(3, 4);
+// console.log(hasil); 
